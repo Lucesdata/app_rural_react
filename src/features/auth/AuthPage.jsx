b6f2ba7d25a251835985bdeb7a3f806172b3381a
Giovanny Guevara Duque <86754797+Lucesdata@users.noreply.github.com>
2025-08-22 11:04:23 +0200

    feat(auth): add navbar and refine background

--- src/features/auth/AuthPage.jsx
+++ src/features/auth/AuthPage.jsx
@@ -1,13 +1,17 @@
 import React from 'react';
 import AuthCard from './components/AuthCard';
+import Navbar from '../../components/Navbar';
 import styles from './auth.module.css';
 
 export default function AuthPage() {
   return (
-    <div className={styles.authPage}>
-      <main className={styles.authContainer}>
-        <AuthCard />
-      </main>
-    </div>
+    <>
+      <Navbar />
+      <div className={styles.authPage}>
+        <main className={styles.authContainer}>
+          <AuthCard />
+        </main>
+      </div>
+    </>
   );
 }
